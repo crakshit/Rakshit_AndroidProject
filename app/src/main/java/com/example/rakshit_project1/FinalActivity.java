4b993169d8455839e4eed62e47392d05921b1d58
Rakshit Chhodavadia <43170545+rakshit33@users.noreply.github.com>
2020-11-17 13:20:30 -0500

    Final to Main Activity Intent added

--- app/src/main/java/com/example/rakshit_project1/FinalActivity.java
+++ app/src/main/java/com/example/rakshit_project1/FinalActivity.java
@@ -2,12 +2,16 @@ package com.example.rakshit_project1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class FinalActivity extends AppCompatActivity {
 
     TextView message, message2;
+    Button shop_again;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +20,20 @@ public class FinalActivity extends AppCompatActivity {
 
         message = findViewById(R.id.message);
         message2 = findViewById(R.id.message2);
+        shop_again = findViewById(R.id.shop_again);
 
         String name = getIntent().getStringExtra("fullname");
         String email = getIntent().getStringExtra("email");
         message.setText("Thank you, " + name);
-        message2.setText("You will get further information on " + email +".");
+        message2.setText("You will receieve further information about your order on " + email +".");
+
+        shop_again.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent main = new Intent(FinalActivity.this, MainActivity.class);
+                startActivity(main);
+            }
+        });
 
 
     }
